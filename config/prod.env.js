cd037e10000328794903582b10912188179ca865
Ben Martin <ben@martindevnow.com>
2018-01-27 00:10:23 -0500

    Updated oauth key for production

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,6 +2,6 @@
 module.exports = {
   NODE_ENV: '"production"',
   BARF_API_BASE: '"http://www.barfbento.com"',
-  BARF_API_SECRET: '"NP7KfNoTdCVdlP3HsLBZcCHzDCwrXasheVVAJYIO"',
+  BARF_API_SECRET: '"mHTlWwMvihZsoyNKkoecshvosxJarxb2SjLbNn27"',
   BARF_API_CLIENT_ID: '"2"',
 }
